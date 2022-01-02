3162f624daab72df3025ba647ec4223f08997423
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-01-02 22:10:20 +0600

    Update item.ts

--- src/_models/item.ts
+++ src/_models/item.ts
@@ -1,24 +1,24 @@
 import { ItemType } from './../_enums/Item-type.enum';
-
+import { Imultimedia } from './multimedia';
 
 export interface Iitem {
-  section:             string;
-  subsection:          string;
-  title:               string;
-  abstract:            string;
-  url:                 string;
-  uri:                 string;
-  byline:              string;
-  item_type:           ItemType;
-  updated_date:        Date;
-  created_date:        Date;
-  published_date:      Date;
+  section: string;
+  subsection: string;
+  title: string;
+  abstract: string;
+  url: string;
+  uri: string;
+  byline: string;
+  item_type: ItemType;
+  updated_date: Date;
+  created_date: Date;
+  published_date: Date;
   material_type_facet: string;
-  kicker:              string;
-  des_facet:           string[];
-  org_facet:           string[];
-  per_facet:           string[];
-  geo_facet:           string[];
-  multimedia:          Multimedia[];
-  short_url:           string;
+  kicker: string;
+  des_facet: string[];
+  org_facet: string[];
+  per_facet: string[];
+  geo_facet: string[];
+  multimedia: Imultimedia[];
+  short_url: string;
 }
