bc92ef85fc65a64989c164d709f2d606297406bf
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-01-02 20:51:20 +0600

    created ui-controls module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
+import { UiControlsModule } from './../_modules/ui-controls/ui-controls.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
 import { TopMenuBarComponent } from './top-menu-bar/top-menu-bar.component';
 import { FooterComponent } from './footer/footer.component';
 
@@ -20,10 +19,7 @@ import { FooterComponent } from './footer/footer.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-
-    MatSliderModule,
-    MatButtonToggleModule
-
+    UiControlsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
