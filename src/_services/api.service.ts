a2a4e065e8d17ed7590e0784d87eadbcaca34921
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-01-02 21:23:26 +0600

    Update api.service.ts

--- src/_services/api.service.ts
+++ src/_services/api.service.ts
@@ -4,35 +4,36 @@ import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
   apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   public get<T>(url: string): Observable<T> {
-      return this.http.get<T>(`${this.apiUrl}${url}`);
+    return this.http.get<T>(`${this.apiUrl}${url}`);
   }
 
   public post<T>(url: string, body): Observable<T> {
-      return this.http.post<T>(`${this.apiUrl}${url}`, body);
+    return this.http.post<T>(`${this.apiUrl}${url}`, body);
   }
 
   public postWithHeader<T>(url: string, body, headers: any): Observable<T> {
-    return this.http.post<T>(`${this.apiUrl}${url}`, body, {headers: headers});
-}
+    return this.http.post<T>(`${this.apiUrl}${url}`, body, {
+      headers: headers,
+    });
+  }
 
   public put<T>(url: string, body): Observable<T> {
-      return this.http.put<T>(`${this.apiUrl}${url}`, body);
+    return this.http.put<T>(`${this.apiUrl}${url}`, body);
   }
 
   public delete<T>(url: string): Observable<T> {
-      return this.http.get<T>(`${this.apiUrl}${url}`);
+    return this.http.get<T>(`${this.apiUrl}${url}`);
   }
 
   public patch<T>(url: string, body: string): Observable<T> {
-      return this.http.patch<T>(`${this.apiUrl}${url}`, body);
+    return this.http.patch<T>(`${this.apiUrl}${url}`, body);
   }
 }
