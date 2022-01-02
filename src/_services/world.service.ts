f545e2bf7a1aa9bada33494c00c3f25d5eb43b1a
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-01-02 21:23:45 +0600

    Create world.service.ts

---
+++ src/_services/world.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { ApiService } from './api.service';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WorldService {
+  private apiPATH = 'world.json?api-key=';
+  private key = environment.apiKey;
+
+  constructor(private api: ApiService) {}
+  getAll() {
+    return this.api
+      .get<any>(`${this.apiPATH}${this.key}`)
+      .pipe(map((res) => res.results));
+  }
+}
