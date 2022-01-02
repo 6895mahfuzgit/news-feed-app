198763848a7bfff816893bcae2dcf455512d3d0f
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-01-02 20:45:04 +0600

    added top-menu-bar in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { TopMenuBarComponent } from './top-menu-bar/top-menu-bar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuBarComponent
   ],
   imports: [
     BrowserModule,
