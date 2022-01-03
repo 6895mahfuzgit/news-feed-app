0eaf011126a7cb1ba785fefd629bb3f0f4d09698
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-01-04 00:16:37 +0600

    Use idb at science service

--- src/_services/science.service.ts
+++ src/_services/science.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { from } from 'rxjs';
 
 import { environment } from '../environments/environment';
 import { ApiService } from './api.service';
 import { Iitem } from '../_models/item';
 import { IResponse } from '../_models/response';
+import { IdbService } from '../_dbs/idb.service';
+import { ItemEnum } from './../_enums/item.enum';
 
 @Injectable({
   providedIn: 'root',
@@ -13,14 +16,22 @@ export class ScienceService {
   private apiPATH = 'science.json?api-key=';
   private key = environment.apiKey;
 
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService, private idbService: IdbService) {
+    this.idbService.connectToIDB(ItemEnum.Science);
+  }
 
   getAll() {
-    return this.api.get<Iitem[]>(`${this.apiPATH}${this.key}`).pipe(
-      map((res) => {
-        let x = res as unknown as IResponse;
-        return x.results;
-      })
-    );
+    if (this.api.getOnlineStatus()) {
+      return this.api.get<Iitem[]>(`${this.apiPATH}${this.key}`).pipe(
+        map((res) => {
+          let x = res as unknown as IResponse;
+          this.idbService.deleteAllItems(ItemEnum.Science);
+          x.results.map((v) => this.idbService.addItems(ItemEnum.Science, v));
+          return x.results;
+        })
+      );
+    } else {
+      return from(this.idbService.getAllData(ItemEnum.Science));
+    }
   }
 }
