f9e246f5a74447a04c54d64de7194506f34ad0b7
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-01-04 00:16:20 +0600

    Use idb at home service

--- src/_services/home.service.ts
+++ src/_services/home.service.ts
@@ -5,6 +5,9 @@ import { environment } from '../environments/environment';
 import { ApiService } from './api.service';
 import { Iitem } from '../_models/item';
 import { IResponse } from '../_models/response';
+import { IdbService } from '../_dbs/idb.service';
+import { ItemEnum } from '../_enums/item.enum';
+import { defer, from, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -13,14 +16,22 @@ export class HomeService {
   private apiPATH = 'home.json?api-key=';
   private key = environment.apiKey;
 
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService, private idbService: IdbService) {
+    this.idbService.connectToIDB(ItemEnum.Home);
+  }
 
   getAll() {
-    return this.api.get<Iitem[]>(`${this.apiPATH}${this.key}`).pipe(
-      map((res) => {
-        let x = res as unknown as IResponse;
-        return x.results;
-      })
-    );
+    if (this.api.getOnlineStatus()) {
+      return this.api.get<Iitem[]>(`${this.apiPATH}${this.key}`).pipe(
+        map((res) => {
+          let x = res as unknown as IResponse;
+          this.idbService.deleteAllItems(ItemEnum.Home);
+          x.results.map((v) => this.idbService.addItems(ItemEnum.Home, v));
+          return x.results;
+        })
+      );
+    } else {
+      return from(this.idbService.getAllData(ItemEnum.Home));
+    }
   }
 }
