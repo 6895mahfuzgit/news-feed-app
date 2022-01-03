b981f12a5511a7c940c333932d14435e947aff58
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-01-04 00:17:04 +0600

    Used idb at  world service

--- src/_services/world.service.ts
+++ src/_services/world.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { from } from 'rxjs';
 
-import { environment } from 'src/environments/environment';
+import { environment } from '../environments/environment';
 import { ApiService } from './api.service';
 import { Iitem } from '../_models/item';
 import { IResponse } from '../_models/response';
+import { IdbService } from '../_dbs/idb.service';
+import { ItemEnum } from './../_enums/item.enum';
 
 @Injectable({
   providedIn: 'root',
@@ -13,14 +16,22 @@ export class WorldService {
   private apiPATH = 'world.json?api-key=';
   private key = environment.apiKey;
 
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService, private idbService: IdbService) {
+    this.idbService.connectToIDB(ItemEnum.World);
+  }
 
   getAll() {
-    return this.api.get<Iitem[]>(`${this.apiPATH}${this.key}`).pipe(
-      map((res) => {
-        let x = res as unknown as IResponse;
-        return x.results;
-      })
-    );
+    if (this.api.getOnlineStatus()) {
+      return this.api.get<Iitem[]>(`${this.apiPATH}${this.key}`).pipe(
+        map((res) => {
+          let x = res as unknown as IResponse;
+          this.idbService.deleteAllItems(ItemEnum.World);
+          x.results.map((v) => this.idbService.addItems(ItemEnum.World, v));
+          return x.results;
+        })
+      );
+    } else {
+      return from(this.idbService.getAllData(ItemEnum.World));
+    }
   }
 }
