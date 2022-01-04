638eac7524dc30f94e381d4cc1152f38ef978a3e
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-01-05 01:58:07 +0600

    Update top-menu-bar.component.ts

--- src/app/top-menu-bar/top-menu-bar.component.ts
+++ src/app/top-menu-bar/top-menu-bar.component.ts
@@ -1,15 +1,20 @@
+import { ItemTypeService } from './../../_services/item-type.service';
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-top-menu-bar',
   templateUrl: './top-menu-bar.component.html',
-  styleUrls: ['./top-menu-bar.component.css']
+  styleUrls: ['./top-menu-bar.component.css'],
 })
 export class TopMenuBarComponent implements OnInit {
+  constructor(private itemTypeService: ItemTypeService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit(): void {
+  loadData(itemType: string) {
+    console.log('set:-',itemType);
+
+    this.itemTypeService.changeSelectedItemType(itemType);
   }
-
 }
