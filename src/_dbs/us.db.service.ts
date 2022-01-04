4f81dcaa0fe1f98fdddb300b96d65d342e16ab63
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-01-05 01:41:18 +0600

    Create us.db.service.ts

---
+++ src/_dbs/us.db.service.ts
@@ -0,0 +1,71 @@
+import { Iitem } from './../_models/item';
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import idb from 'idb';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsDbService {
+
+  private _dataChange: Subject<Iitem> = new Subject<Iitem>();
+  private _dbPromise;
+
+  constructor() {}
+
+  connectToIDB(table: string) {
+
+    this._dbPromise = idb.open('pwa-database-us', 1, (UpgradeDB) => {
+      if (!UpgradeDB.objectStoreNames.contains(table)) {
+        UpgradeDB.createObjectStore(table, {
+          keyPath: 'id',
+          autoIncrement: true,
+        });
+      }
+    });
+  }
+
+  addItems(target: string, value: Iitem) {
+    this._dbPromise.then((db: any) => {
+      const tx = db.transaction(target, 'readwrite');
+      tx.objectStore(target).put(value);
+      this.getAllData(target).then((items: Iitem) => {
+        this._dataChange.next(items);
+      });
+      return tx.complete;
+    });
+  }
+
+  deleteItems(target: string, value: Iitem) {
+    this._dbPromise.then((db: any) => {
+      const tx = db.transaction(target, 'readwrite');
+      const store = tx.objectStore(target);
+      store.delete(value);
+      this.getAllData(target).then((items: Iitem) => {
+        this._dataChange.next(items);
+      });
+      return tx.complete;
+    });
+  }
+
+  deleteAllItems(target: string) {
+    this._dbPromise.then((db: any) => {
+      const tx = db.transaction(target, 'readwrite');
+      tx.objectStore(target).clear();
+      return tx.complete;
+    });
+  }
+
+  getAllData(target: string) {
+    return this._dbPromise.then((db: any) => {
+      const tx = db.transaction(target, 'readonly');
+      const store = tx.objectStore(target);
+      return store.getAll();
+    });
+  }
+
+  dataChanged(): Observable<Iitem> {
+    return this._dataChange;
+  }
+
+}
