b1c408d3f10fd53c3f30ddf349eb5e8e8561e448
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-01-04 22:56:08 +0600

    Create item-type.service.ts

---
+++ src/_services/item-type.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ItemTypeService {
+  private itemTypeSubject = new BehaviorSubject<string>('');
+  itemType$ = this.itemTypeSubject.asObservable();
+
+  constructor() {}
+
+  changeSelectedItemType(itemType: string) {
+    this.itemTypeSubject.next(itemType);
+  }
+}
