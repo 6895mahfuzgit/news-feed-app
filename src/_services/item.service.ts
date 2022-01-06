bdc994b048b1540f54f730205b7abcf4a0f749a2
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-01-06 23:41:40 +0600

    Update item.service.ts

--- src/_services/item.service.ts
+++ src/_services/item.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ItemService {
+
+  private urlSubject = new BehaviorSubject<string>('');
+  url$ = this.urlSubject.asObservable();
+
   constructor() {}
+
+  changeURL(url:string){
+    this.urlSubject.next(url);
+   }
 }
