98f6e21f1bd6827b3048d551459348cf581d2122
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-01-06 23:40:26 +0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
-import { SafeURLPipe } from './../_pipes/safe.pipe.';
-import { UiControlsModule } from './../_modules/ui-controls/ui-controls.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,6 +10,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { ItemListComponent } from './item-list/item-list.component';
+import { SafeURLPipe } from './../_pipes/safe.pipe.';
+import { UiControlsModule } from './../_modules/ui-controls/ui-controls.module';
+import { PopupDialogComponent } from './popup-dialog/popup-dialog.component';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { ItemListComponent } from './item-list/item-list.component';
     TopMenuBarComponent,
     FooterComponent,
     ItemListComponent,
-    SafeURLPipe
+    SafeURLPipe,
+    PopupDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -31,6 +33,6 @@ import { ItemListComponent } from './item-list/item-list.component';
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
