c33b2d3e717c263962282682f49c78407e9a1f6e
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-01-06 23:39:45 +0600

    Update ui-controls.module.ts

--- src/_modules/ui-controls/ui-controls.module.ts
+++ src/_modules/ui-controls/ui-controls.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatDialogModule} from '@angular/material/dialog';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [],
@@ -14,13 +14,17 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     MatSliderModule,
     MatButtonToggleModule,
     MatProgressSpinnerModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatDialogModule,
+    MatButtonModule,
   ],
   exports:[
     MatSliderModule,
     MatButtonToggleModule,
     MatProgressSpinnerModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatDialogModule,
+    MatButtonModule,
   ]
 })
 export class UiControlsModule { }
